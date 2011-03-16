2e50db0827ca5a02f563876e5a3dce42e0891ff8
denger <denger@denger.pp.ru>
2011-03-16 22:03:13 +0600

    fix setup.py long description

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@
 from distutils.core import setup
 
 import sys
+import os
 reload(sys).setdefaultencoding("UTF-8")
+
+def get_long_desc():
+    setup_dir = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(setup_dir, 'README.md')
+    return open(filename).read().decode('utf8')
 
 setup(
     name='django-onpay',
@@ -17,7 +23,7 @@ setup(
     download_url = 'http://bitbucket.org/denger/django-onpay/get/tip.zip',
     license = 'MIT license',
     description = u'Приложение для интеграции платежной системы ONPAY в проекты на Django.'.encode('utf8'),
-    long_description = open('README.md').read().decode('utf8'),
+    long_description = get_long_desc(),
 
     classifiers=(
         'Development Status :: 4 - Beta',
